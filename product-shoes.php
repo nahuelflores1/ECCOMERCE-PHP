9e6106995c8f0d320302f022ebf5acfeb9181981
nahuelflores1 <nahuelflores2018@hotmail.com>
2023-05-02 10:39:05 -0300

    Arreglos

--- product-shoes.php
+++ product-shoes.php
@@ -19,9 +19,9 @@ require 'config/config.php'
         crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <link rel="stylesheet" href="http://localhost/ECCOMERCE/css/main.css">
     <link rel="stylesheet" href="http://localhost/ECCOMERCE/css/glider.min.css">
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <title>KicksMarket</title>
 </head>
 
@@ -129,9 +129,9 @@ require 'config/config.php'
                         ?>
                         <div class="col-md-6" style="width:30vh;">
                             <div class="card-deck">
-                                <div class="card">
+                                <div class="card" style="">
                                     <img  src="data:images/jpg;base64, <?php echo base64_encode($row['product_image']);  ?>" class="card-img-top">
-                                    <div>
+                                    <div style="height:100px; width:90%; ">
                                         <h6 style="cursor:pointer;" class="card-title text-center"><?= $row['product_name']; ?></h6>
                                     </div>
                                     <div class="text-price">
