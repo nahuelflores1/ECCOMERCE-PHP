99ab5e2809c5d964db5f2c3cddb82cea2c467ea8
nahuelflores1 <nahuelflores2018@hotmail.com>
2023-05-11 10:28:27 -0300

    jsc de login y register funcionando

--- js/script.js
+++ js/script.js
@@ -1,2 +1,14 @@
-const loginForm = document.querySelector('#login-form');
-const registerForm = document.querySelector('#register-form');
+const loginForm = document.getElementById("login-form");
+const registerForm = document.getElementById("register-form");
+const loginLink = document.querySelector(".login-link");
+const registerLink = document.querySelector(".register-link");
+
+loginLink.addEventListener("click", () => {
+  loginForm.style.display = "block";
+  registerForm.style.display = "none";
+});
+
+registerLink.addEventListener("click", () => {
+  loginForm.style.display = "none";
+  registerForm.style.display = "block";
+});
